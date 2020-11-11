39af8e64fef17f493f40f8f83647202f0f00b741
Andre <itstheandre@gmail.com>
2020-11-11 12:45:38 +0100

    Express

--- movies-example/server.js
+++ movies-example/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const hbs = require("hbs");
+const moviesJson = require("./movies.json");
 
 const app = express();
 
 app.set("view engine", "hbs");
 
-app.get("/", (req, res) => res.render("index"));
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.get("/", (req, res) =>
+  res.render("index", {
+    movies: moviesJson,
+    isArrayOfMovies: "Of course it is",
+    isStudent: false,
+    hobbies: [],
+  })
+);
 
 app.listen(3001, () => console.log(`LISTENING ON PORT 3001`));
